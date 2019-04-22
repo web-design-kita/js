4dba7456e3f3f6a9edac7cb31bec8883700952be
fitraganteng <49879180+fitraganteng@users.noreply.github.com>
2019-04-23 00:19:02 +0700

    Create scripcode.js

---
+++ scripcode.js
@@ -0,0 +1,9 @@
+// by webdesainkita.blogspot.com
+/* <![CDATA[ */
+document.write('<div class="\x73\x61\x6E\x74\x61-mars">');
+document.write('<ul id="navigation"><a href="#x" id="link-x">X</a>C'+'red'+'it</ul>');
+document.write('<table cellpadding="10" cellspacing="10"><tbody><tr>');
+document.write('<center><span id="textDestination" rel="nofollow" style="margin: 0px"/></center>');
+document.write('</tr></tbody></table>');
+document.write('</div>');
+/* ]]> */
